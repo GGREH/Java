10768ea4b5f0a33d8b200de7b4640ced8c01e64d
GREH <roma.class08@gmail.com>
2025-09-30 10:28:00 +0300

    LR2.Com..

--- S1/In.java
+++ S1/In.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 
 public class In {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Input value of a: ");
-        var a = new Scanner(System.in).nextInt();
+        var a = scanner.nextInt();
         System.out.println(a);
 
         System.out.print("Input value of d: ");
-        var d = new Scanner(System.in).nextDouble();
+        var d = scanner.nextDouble();
         System.out.println(d);
 
         System.out.print("Input value of b: ");
-        var b = new Scanner(System.in).nextBoolean();
+        var b = scanner.nextBoolean();
         System.out.println(b);
 
         System.out.print("Input value of ch: ");
-        var ch = new Scanner(System.in).nextLine().charAt(0);
+        var ch = scanner.nextLine().charAt(0);
         System.out.println(ch);
 
         System.out.print("Input value of s: ");
-        var s = new Scanner(System.in).nextLine();
+        var s = scanner.nextLine();
         System.out.println(s);
+
+        scanner.close();
     }
 }
